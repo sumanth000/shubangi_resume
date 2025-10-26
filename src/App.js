8aee39111f277bcafbc7f194b9e027da5fa2370c
sumanth000 <109060864+sumanth000@users.noreply.github.com>
2025-10-26 16:11:25 +0530

    chagnes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { motion } from "framer-motion";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
